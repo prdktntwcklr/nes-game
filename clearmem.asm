d44c72f6c7789016aeae15b68c097906ff35b6ac
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2023-01-19 23:46:14 +0800

    feat: add housekeeping

--- clearmem.asm
+++ clearmem.asm
@@ -7,11 +7,30 @@
 .byte $4E,$45,$53,$1A        ; 4 bytes with the characters 'N','E','S','\n'
 .byte $02                    ; how many 16kB of PRG-ROM we use (=32kB)
 .byte $01                    ; how many 8kB of CHR-ROM we use (=8kB)
-.byte %00000000              ; Horz mirroring, no bat, mapper 0
-.byte %00000000              ; Mapper 0, playchoice, NES 2.0
-.byte $00                    ; No PRG-RAM
+.byte %00000000              ; horz mirroring, no bat, mapper 0
+.byte %00000000              ; mapper 0, playchoice, NES 2.0
+.byte $00                    ; no PRG-RAM
 .byte $00                    ; NTSC TV format
-.byte $00                    ; No PRG-RAM
-.byte $00,$00,$00,$00,$00    ; Unused padding to complete 16 bytes of header
+.byte $00                    ; no PRG-RAM
+.byte $00,$00,$00,$00,$00    ; unused padding to complete 16 bytes of header
 
 .segment "CODE"
+.org $8000
+
+RESET:
+    sei                      ; disable all IRQ interrupts
+    cld                      ; clear the decimal mode flag
+    ldx #$FF
+    txs                      ; initialize stack pointer to $01FF
+
+NMI:
+    rti                      ; return from interrupt
+
+IRQ:
+    rti                      ; return from interrupt
+
+.segment "VECTORS"
+.org $FFFA
+.word NMI                    ; address of the NMI handler
+.word RESET                  ; address of the RESET handler
+.word IRQ                    ; address of the IRQ handler
